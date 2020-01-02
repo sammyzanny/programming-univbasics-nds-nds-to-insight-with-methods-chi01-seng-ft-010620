7c0d838abdff937a400f229691155981481e245a
Samuel Zandi <samuelczandi@gmail.com>
2020-01-02 20:13:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,15 +17,15 @@ end
 # The return value should be like:
 #
 # { directorOne => allTheMoneyTheyMade, ... }
+
 def directors_totals(nds)
-{"Stephen Spielberg"=>1357566430,
-        "Russo Brothers"=>2281002470,
-        "James Cameron"=>2571020373,
-        "Spike Lee"=>256624217,
-        "Wachowski Siblings"=>806180282,
-        "Robert Zemeckis"=>1273838385,
-        "Quentin Tarantino"=>662738268,
-        "Martin Scorsese"=>636812242,
-        "Francis Ford Coppola"=>509719258
-}
+  result = {}
+  director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
+
